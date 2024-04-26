d973c2a506402c5e9cdb2a4d30cfb219c415f613
faresdu <443102276@student.ksu.edu.sa>
2024-04-26 22:22:20 +0300

    update moore

--- client.py
+++ client.py
@@ -1,5 +1,6 @@
 import socket
 import pickle
+import sys
 port = 12388
 ip = 'localhost'
 
@@ -7,23 +8,42 @@ ip = 'localhost'
 
 
 def userInterface(sock):
-    print('\033[1;34;40m Welcome to Networant!')
-    print('\033[1;37;40m Please select who are you as shown below:')
-    print('1- Owner\n2- Customer\n', end= ' ')
-    selection = input()
-    if selection == '1':
-        ownerAuth(sock)
-    elif selection == '2':
-        sock.sendall(b'Customer')
+    flag = True
+    for i in range(2):
+        print('\033[1;34;40m <#> Welcome to Networant!')
+        print('\033[1;37;40m <#> Please select who are you as shown below:')
+        print('\033[1;33;40m <#> 1- Owner\n<#> 2- Customer\n<#> 3- Exit\n\033[1;37;40m ->', end= ' ')
+        selection = ''
+        selection = input('->')
+        if selection == '1':
+            ownerAuth(sock)
+        elif selection == '2':
+            sock.sendall(b'Customer,')
+            retrieveMenu(sock)
+        elif selection == '3':
+            print('<#> Thanks for dealing with Networant, Goodbye!')
+            sock.close()
+            flag = False
+
+def retrieveMenu(sock):
+    data = sock.recv(1024)
+    data = pickle.loads(data)
+    for i,k in data.items():
+        print(i,":",k['price'])
+    
+
 def ownerAuth(sock):
     try:
         print('Enter your username:')
         username = input()
         print('Enter your password:')
         password = input()
-        sock.sendall(b'Owner')
+        x= ''
+        x= 'Owner'+','+username + ',' + password
+        sock.sendall(x.encode())
     except:
-        
+        print("Username or password is not correct.")
+
 def main():
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     print('socket initiated')
@@ -32,15 +52,9 @@ def main():
         sock.connect((ip,port))
         print('socket connected')
         userInterface(sock)
-        data = sock.recv(1024)
-        data = pickle.loads(data)
-        for i,k in data.items():
-            print(i,":",k['price'])
+    except Exception as e:
+        print('Error on client side:', str(e))
 
-            
-    except:
-        print('eroor on client side')
-   
 
 if __name__ == '__main__':
     main()
--- server.py
+++ server.py
@@ -5,12 +5,51 @@ import pickle
 port = 12388
 ip = 'localhost'
 
-    
+
 def load_menu(file_path):
     with open(file_path, 'r') as file:
         menu_data = json.load(file)
     return menu_data
+def add_item_to_json(file_path, item_name, item_price, item_quantity):
+ 
+    data =load_menu('menu.json')
+    data[item_name] = {
+        "price": item_price,
+        "quantity": item_quantity
+    }
 
+    with open(file_path, 'w') as file:
+        json.dump(data, file, indent=4)
+def modifyItem():
+    menu_data = load_menu('menu.json')
+    print('What item do you want to modify?\n')
+    modifiedItem = input('->')
+
+    if modifiedItem in menu_data:
+        print('What is the new price of the item?\n')
+        newPrice = input('->')
+        print('What is the new quantity of the item?\n')
+        newQuantity = input('->')
+
+        menu_data[modifiedItem]['price'] = newPrice
+        menu_data[modifiedItem]['quantity'] = newQuantity
+
+        with open('menu.json', 'w') as file:
+            json.dump(menu_data, file)
+
+        print('Item modified successfully!')
+    else:
+        print('Item not found in the menu.')
+def addItem():
+    addToMenu = load_menu('menu.json')
+    print('What item do you want to add?\n')
+    addedItem = input('->')
+    print('What is the price of the item?\n')
+    itemPrice = input('->')
+    print('What is the quantity of the item?\n')
+    itemQuantity = input('->')
+    addedItemJSON = add_item_to_json('menu.json',addedItem,itemPrice,itemQuantity)
+    print('Item added successfuly!')
 def main():
     try:
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -25,24 +64,33 @@ def main():
             conn, addr = sock.accept()
             print('socket connected')
             data = conn.recv(1024).decode()
-            print(data)
-            if data == 'Customer':
+            k=data.split(',')
+            print('meow')
+            if k[0] == 'Customer':
+                print('hanzo')
                 menu = load_menu('menu.json')
+                print('d7man')
                 menu = pickle.dumps(menu)
+                print('fares 3mkm')
                 conn.sendall(menu)
-            elif data == 'Owner':
-                username = 'admin'
-                password = 'admin'
-                
-                
-                
-        
+                print('3ziz')
+            elif k[0] == 'Owner':
+                if k[1]=='admin' and k[2]=='admin':
+                    print('Hello dear Owner of Networant!')
+                    print('Tell me what do you want to do?')
+                    print('1- Add items to the menu\n2- Modify prices or quantity')
+                    selection = input('->')
+                    if selection == '1':
+                        addItem()
+                    elif selection == '2':
+                        modifyItem()
+                else:
+                    print('\033[1;31;40m Username or password is incorrect.')
+            else:
+                print('howowow')
+
     except:
-        print("eroorrrrrr on server side")
-    finally:
-        sock.close()
-    
+        print("\033[1;31;40m Error on server side")
 
 if __name__ == '__main__':
     main()
-
